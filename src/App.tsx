834b4ab09b19e86277572936d0ceab35a47c73aa
Gadelha <gadelha.ti@gmail.com>
2022-01-27 01:09:24 -0300

    Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,23 @@
+import { useState } from "react";
+import AppRoutes from "./AppRoutes";
+import Observation from "./components/Observation";
+
 export default function App() {
-  return <h1>Hello World</h1>
+  const [observation, setObservation] = useState<string[]>([
+    'Observation 1',
+    'Observation 2',
+    'Observation 3'
+  ])
+  function create() {
+    setObservation([...observation, 'Observation 4'])
+  }
+  return (
+    <>
+      <AppRoutes/>
+      {observation.map(observation => {
+        return <Observation mimi={observation}/>
+      })}
+      <button style={{backgroundColor: 'GrayText'}} onClick={create}>Button</button>
+    </>
+  )
 }
---
+++ src/components/Observation.tsx
@@ -0,0 +1,97 @@
+type Observation = {
+    id?: string,
+    // coordinates?: string;
+    mimi: string;
+    // mjmj?: string;
+    ddddddd?: string;
+    a1?: string;
+    bw?: string;
+    nbnbnb?: string;
+    yy?: string;
+    gg?: string;
+    iw?: string;
+    ii?: string;
+    iii?: string;
+    lalala?: string;
+    qc?: string;
+    lolololo?: string;
+    ir?: string;
+    ix?: string;
+    h?: string;
+    vv?: string;
+    n?: string;
+    dd?: string;
+    ff?: string;
+    fff?: string;
+    sn1_1?: string;
+    ttt?: string;
+    sn2_1?: string;
+    tdtdtd?: string;
+    p0p0p0p0?: string;
+    pppp?: string;
+    a3?: string;
+    hhh?: string;
+    a?: string;
+    ppp?: string;
+    rrr?: string;
+    tr?: string;
+    ww?: string;
+    w1w2?: string,
+    w1?: string;
+    w2?: string;
+    wawa?: string;
+    wa1?: string;
+    wa2?: string;
+    nh?: string;
+    cl?: string;
+    cm?: string;
+    ch?: string;
+    gggg?: string;
+    ds?: string;
+    vs?: string;
+    ss?: string;
+    twtwtw?: string;
+    pwapwa?: string;
+    hwahwa?: string;
+    pwpw?: string;
+    hwhw?: string;
+    dw1dw1?: string;
+    dw2dw2?: string;
+    pw1pw1?: string;
+    hw1hw1?: string;
+    pw2pw2?: string;
+    hw2hw2?: string;
+    is?: string;
+    eses?: string;
+    rs?: string;
+    hwahwahwa?: string;
+    sw?: string;
+    tbtbtb?: string;
+    ci?: string;
+    si?: string;
+    bi?: string;
+    di?: string;
+    zi?: string;
+    sn1_3?: string;
+    txtxtx?: string;
+    sn2_3?: string;
+    tntntn?: string;
+    _89?: string;
+    p24p24p24?: string;
+    ichw?: string;
+    icm?: string;
+    cs?: string;
+    icf?: string;
+    icp?: string;
+    icq?: string;
+    // observador?: Observator;
+    dataObservacao?: Date;
+    // estacao?: Station;
+    // file?: string;
+}
+
+export default function Observation(props: Observation) {
+    return(
+        <>{props.mimi}</>
+    );
+}
