2a9a65fae4ab69c87635466c238371a0198abd4d
Gadelha <gadelha.ti@gmail.com>
2022-02-07 03:05:49 -0300

    Simple axios

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -6,25 +6,12 @@ const token = getLocalAccessToken();
 export const api = axios.create({
   // baseURL: "https://10.5.193.20/application-back",
   baseURL: "http://127.0.0.1:3128/application-back",
-  // responseType: 'json',
-  headers:{
-    // 'Content-Type': 'application/json'
-    // 'Access-Control-Allow-Origin': '*',
-    // 'Authorization': `Bearer ${token}`,
-  }
 });
 
 api.interceptors.request.use(async config => {
   const token = getLocalAccessToken();
   if (token) {
     config.headers!.Authorization = `Bearer ${token}`;
-    // config.headers.Access = `*`,
-    // config.headers.Content = `application/json`
   }
-  // if (token) {
-  //   axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-  // } else {
-  //   delete axios.defaults.headers.common['Authorization'];
-  // }
   return config;
 })
